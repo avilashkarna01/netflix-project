64e2db7a6be371de16a6ee3ae51578a3f6898a3f
[rajathack007] <[manishakumari993424@gmail.com]>
2021-07-22 19:00:25 +0530

    my first netflix clone

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className= "TITLE"> 
+        <h2> Design by Abhilash Karna</h2>
+        </div>
+    )
+}
+
+export default Footer;
